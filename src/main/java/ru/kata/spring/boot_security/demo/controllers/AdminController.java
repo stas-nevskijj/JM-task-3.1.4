b9491e79da49f54c2636a16fc75a8188b70125e4
Stas Nevski <stas-nevskijj@mail.ru>
2022-10-28 14:27:45 +0300

    Init

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/AdminController.java
@@ -53,16 +53,16 @@ public class AdminController {
         return "bootstrap4/admin";
     }
 
-//    @GetMapping("/new")
-//    public String createUser(@ModelAttribute(name = "user") User user) {
-//        return "redirect:/admin";
-//    }
+    @GetMapping("/new")
+    public String createUser(@ModelAttribute(name = "user") User user) {
+        return "redirect:/admin";
+    }
 
-//    @PostMapping
-//    public String create(@ModelAttribute(name = "user") User user) {
-//        userService.save(user);
-//        return "redirect:/admin";
-//    }
+    @PostMapping
+    public String create(@ModelAttribute(name = "user") User user) {
+        userService.save(user);
+        return "redirect:/admin";
+    }
 
     @GetMapping("/{id}/edit")
     public String edit(@PathVariable(name = "id") int id,
@@ -71,16 +71,16 @@ public class AdminController {
         return "bootstrap4/admin";
     }
 
-//    @PatchMapping("/{id}")
-//    public String update(@PathVariable("id") int id,
-//                         @ModelAttribute(name = "user") User user) {
-//        userService.update(id, user);
-//        return "redirect:/admin";
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public String delete(@PathVariable(name = "id") int id) {
-//        userService.delete(id);
-//        return "redirect:/admin";
-//    }
+    @PatchMapping("/{id}")
+    public String update(@PathVariable("id") int id,
+                         @ModelAttribute(name = "user") User user) {
+        userService.update(id, user);
+        return "redirect:/admin";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable(name = "id") int id) {
+        userService.delete(id);
+        return "redirect:/admin";
+    }
 }
